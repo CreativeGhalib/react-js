c6780ac77ac3478eac140c12504dbd660e2b8bac
CreativeGhalib <creativeghalib@gmail.com>
2026-03-13 17:44:33 +0600

    Learnt how to export default and import

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import Library from "./library"
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Library from './library';
+import './App.css'
 
 function App() {
+  const books = [
+    { id: 1, name: 'The Great Gatsby', price: 10.99 },
+    { id: 1, name: 'The vue book', price: 11.99 },
+    { id: 1, name: 'The react nbook', price: 12.99 },
+    { id: 1, name: 'The book book', price: 12.99 },
+  ];
+
 
 
   return (
     <>
+
+      <h1>React core concept</h1>
+      <Library books={books}></Library>
+
+
+
 
 
     </>
--- src/library.jsx
+++ src/library.jsx
@@ -1,8 +1,9 @@
-export default function Library() {
+export default function Library({books}) {
   return (
     <div>
       <h2>My national Central Library</h2>
+      <p>book collection: {books.length}</p>
       <p>Address:</p>
     </div>
-  )
+  );
 }
