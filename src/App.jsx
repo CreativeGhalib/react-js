51c8811b38225d7a40a5bfd57ec82561675938d8
CreativeGhalib <creativeghalib@gmail.com>
2026-03-15 00:36:04 +0600

    ow i know how to do dynamic rendering using  props in function

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ function App() {
       <Person></Person>
       <Sports></Sports>
       <Student></Student>
+      <Developer name="mozumdar" tech="JavaScript" ></Developer>
+      <Developer name="shabana" tech="Python" ></Developer>
+      <Developer name="yaali" tech="Java" ></Developer>
 
     </>
   );
@@ -16,11 +19,15 @@ function Person() {
   const age = 17;
   const name="jolil"
   // return <p>I am a person and my age is {age}</p>;
-  return <p>My name is {name} and my age is {age}</p>;
+  return (
+    <p style={{ border: '2px solid red', padding: '10px', borderRadius: '5px' }}>
+      My name is {name} and my age is {age}
+    </p>
+  );
 }
 function Sports() {
   return (
-    <section>
+    <section className='student'>
       <h3>Cricket</h3>
       <ol>
         <li>Kola</li>
@@ -32,11 +39,31 @@ function Sports() {
 }
 function Student() {
   return (
-   <div>
-      <p>Name: </p>
-      <p>Dep: </p>
+   <div style ={Personstyle}>
+      <p>Name: John Doe</p>
+      <p>Dep: Computer Science</p>
    </div>
   )
+
 }
+
+const Personstyle = {
+  color: 'blue',
+  backgroundColor: 'yellow',
+  padding: '10px',
+  borderRadius: '5px',
+  border: '4px solid yellow',
+  fontSize: '40px'
+}
+function Developer(props) {
+  console.log(props)
+  return (
+    <div style={{border: '4px solid yellow'}}>
+      <p>Developer:{props.name}</p>
+      <p>Technology: {props.tech}</p>
+    </div>
+  )
+}
+
 
 export default App;
