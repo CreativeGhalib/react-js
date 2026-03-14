39ca0757af5a1720e89c4cb0705a0bef12963d47
CreativeGhalib <creativeghalib@gmail.com>
2026-03-14 22:07:36 +0600

    revising lessons from 31.3 with examples

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Library from './library';
-import './App.css'
+import './App.css';
 
 function App() {
-  const books = [
-    { id: 1, name: 'The Great Gatsby', price: 10.99 },
-    { id: 1, name: 'The vue book', price: 11.99 },
-    { id: 1, name: 'The react nbook', price: 12.99 },
-    { id: 1, name: 'The book book', price: 12.99 },
-  ];
-
-
-
   return (
     <>
-
-      <h1>React core concept</h1>
-      <Library books={books}></Library>
-
-
-
-
+      <h1>Mesbah Ghalib</h1>
+      <Person></Person>
+      <Person></Person>
 
     </>
-  )
+  );
 }
-
-export default App
+function Person() {
+  const age = 17;
+  return <p>I am a person and my age is {age}</p>;
+}
+export default App;
