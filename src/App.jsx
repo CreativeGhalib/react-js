1603c2ae738eea3edd8bdda57281f22a5ed44441
CreativeGhalib <creativeghalib@gmail.com>
2026-03-14 23:57:13 +0600

    learning about how to use class ,when we retun output using jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,37 @@ function App() {
       <h1>Mesbah Ghalib</h1>
       <Person></Person>
       <Person></Person>
+      <Sports></Sports>
+      <Student></Student>
 
     </>
   );
 }
 function Person() {
   const age = 17;
-  return <p>I am a person and my age is {age}</p>;
+  const name="jolil"
+  // return <p>I am a person and my age is {age}</p>;
+  return <p>My name is {name} and my age is {age}</p>;
 }
+function Sports() {
+  return (
+    <section>
+      <h3>Cricket</h3>
+      <ol>
+        <li>Kola</li>
+        <li>Banana</li>
+      </ol>
+    <p>Playsing & losing</p>
+  </section>
+  )
+}
+function Student() {
+  return (
+   <div>
+      <p>Name: </p>
+      <p>Dep: </p>
+   </div>
+  )
+}
+
 export default App;
